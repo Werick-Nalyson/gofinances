90f12eab1319d314baa9d8e30cd6d0cc74f0b4f4
werick <werick@pop-os.localdomain>
2022-01-30 00:31:43 -0300

    header init

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import { Container, Title } from './styles'
+import { Feather } from '@expo/vector-icons'
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGretting,
+    UserName,
+    Icon
+} from './styles'
 
 export function Dashboard () {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://media-exp1.licdn.com/dms/image/C4E03AQHuFwvjCgyr9w/profile-displayphoto-shrink_800_800/0/1599278928625?e=1649289600&v=beta&t=UHm5GEDcYLYTreKBtC5WgEVADuAf5At-Tno80FFMiqc' }}
+                        />
+
+                        <User>
+                            <UserGretting>Olá, </UserGretting>
+                            <UserName>Werick</UserName>
+                        </User>
+                    </UserInfo>
+                    
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
